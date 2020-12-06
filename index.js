57912e3db73b0b3c8a279425855a8a6bab1379a7
Nick Ferrell <nickmwferrell@gmail.com>
2020-12-05 23:46:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import STORE from './STORE';
+
+ReactDOM.render(
+  <App store={STORE} />,
+  document.querySelector('#root'));
