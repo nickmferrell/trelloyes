752897f99ee338f8d8f1f78ea23ef541174d270a
Nick Ferrell <nickmwferrell@gmail.com>
2020-12-07 18:39:57 -0500

    Updated tests

--- List.test.js
+++ List.test.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import List from './List';
 import renderer from 'react-test-renderer'
 
+describe('List component', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<List />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
-    ReactDOM.unmountComponentAtNode(div);
-})
-
-it('renders the UI as expected', () => {
-    const tree = renderer
-        .create(<List name="List"/>)
-        .toJSON()
-    expect(tree).toMatchSnapshot();
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<List name="List"/>)
+            .toJSON()
+        expect(tree).toMatchSnapshot();
+    })
 })
